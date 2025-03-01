d77bd8ad58220299fc3d2f31b620886d9dabbbe9
leontoys <svliyon@gmail.com>
2025-03-02 08:25:01 +1100

    created-backend-frontend

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,30 @@ const mongoose = require('mongoose')
 const cors = require('cors')
 const dotenv = require('dotenv')
 
-//use the modules
-dotenv.config
+//set up
+dotenv.config()
 const app = express()
+const PORT = process.env.PORT || 5001
+const url = process.env.MONGODB_URI
+console.log("url",url)
+
+//use middleware
+app.use(cors())
+app.use(express.json())
+
+//db connect
+mongoose.connect(url)
+.then(()=>console.log('MongoDB connected'))
+.catch((error)=>console.error(error))
+
+//base
+app.get('/',(req,res)=>{
+    res.send('Hello')
+})
+
+//start server
+app.listen(PORT,()=>{
+    console.log(`server running on http://localhost:${PORT}/`)
+})
+
+
