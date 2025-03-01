26eebb4743c2f17417dc0d15bc105336a05fcd61
leontoys <svliyon@gmail.com>
2025-03-02 08:08:12 +1100

    test-gitignore

---
+++ backend/index.js
@@ -0,0 +1,9 @@
+//load modules
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const dotenv = require('dotenv')
+
+//use the modules
+dotenv.config
+const app = express()
