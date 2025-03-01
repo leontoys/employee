204b75a3ee53c2ae13d29384a8c134ba8d2baa21
leontoys <svliyon@gmail.com>
2025-03-02 08:59:35 +1100

    created-backend-employee-service-api

--- backend/index.js
+++ backend/index.js
@@ -20,9 +20,62 @@ mongoose.connect(url)
 .then(()=>console.log('MongoDB connected'))
 .catch((error)=>console.error(error))
 
+//create Schema, Model etc
+const EmployeeSchema = new mongoose.Schema({
+    name : String,
+    position : String,
+    level : String
+})
+//model
+const Employee = mongoose.model("Employee",EmployeeSchema)
+
 //base
 app.get('/',(req,res)=>{
     res.send('Hello from NodeJS')
+})
+
+//routes
+//get all employees
+app.get('/employees',async (req,res)=>{
+    const employees = await Employee.find({})
+    res.status(200).json(employees)
+})
+
+//get a specific employee
+app.get('/employees/:id',async(req,res)=>{
+    const employee = await Employee.findById(req.params.id)
+    res.status(200).json(employee)
+})
+
+//create a new employee
+app.post('/employees',async(req,res)=>{
+    const newEmployee = new Employee({
+        name : req.body.name,
+        position : req.body.position,
+        level : req.body.level
+    })
+    const savedEmployee = await newEmployee.save()
+    res.status(201).json(savedEmployee)
+})
+
+//update an employee
+app.patch('/employees/:id',async(req,res)=>{
+    const updatedEmployee = await Employee.findByIdAndUpdate(
+        req.params.id,
+        {
+            name : req.body.name,
+            position : req.body.position,
+            level : req.body.level
+        },
+        {new : true} //return the updated document
+    )
+    return res.status(200).json(updatedEmployee)
+})
+
+//delete by id
+app.delete('/employees/:id',async(req,res)=>{
+    const deletedEmployee = await Employee.findByIdAndDelete(req.params.id)
+    res.status(200).json(deletedEmployee)
 })
 
 //start server
