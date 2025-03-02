d0811f3f20d3e5d630b78a8595ae93cd975da872
leontoys <svliyon@gmail.com>
2025-03-02 11:28:21 +1100

    add-new-employee-form-completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,7 @@ import axios from "axios";
 function App() {
   const baseUrl = "http://localhost:5000"
   const [employees,setEmployees] = useState([])
+  const [newEmployee,setNewEmployee] = useState({name:"",position:"",level:""})
 
   const fetchEmployees = async()=>{
     try {
@@ -19,6 +20,17 @@ function App() {
 
   }
 
+  const addEmployee = async (event)=>{
+    event.preventDefault()
+    try {
+      const response = await axios.post(`${baseUrl}/employees`,newEmployee)
+      setEmployees(employees.concat(response.data))//add new employee
+      setNewEmployee({name:"",position:"",level:""})
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
   //get all employees
   useEffect(()=>{
     fetchEmployees()
@@ -27,6 +39,34 @@ function App() {
   return (
     <div>
       <h1>Employee Management App</h1>
+      <div>
+        <h2>Add New Employee</h2>
+        <form onSubmit={addEmployee}>
+          <div>
+          <label>Name : </label>
+          <input id="name" 
+          value = {newEmployee.name}
+          onChange={(e)=>setNewEmployee({...newEmployee,name:e.target.value})}
+          ></input>            
+          </div>
+          <div>
+          <label>Position : </label>
+          <input id="position"
+          value={newEmployee.position}
+          onChange={(e)=>setNewEmployee({...newEmployee,position:e.target.value})}
+          ></input>            
+          </div>
+          <div>
+          <label>Level : </label>
+          <input id="level"
+          value={newEmployee.level}
+          onChange={(e)=>setNewEmployee({...newEmployee,level:e.target.value})}
+          ></input>
+          </div>
+          <button type="submit">Add Employee</button>
+        </form>
+      </div>
+      <div>
       <h2>Employee List</h2>
       <ul>
         {employees.map(employee=>{ return (
@@ -34,7 +74,8 @@ function App() {
             {employee.name} - {employee.position} - {employee.level}
           </li>
         )})}
-      </ul>
+      </ul>    
+      </div>
     </div>
   )
 }
